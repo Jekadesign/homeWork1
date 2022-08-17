313deec1f952542696862047af5b00d591a8d1d9
Евгений К <direct.ev@gmail.com>
2022-08-17 23:08:38 +0300

    2

---
+++ TestProject1/PageObjects/MainGooglePageObject.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.PageObjects
+{
+	internal class MainGooglePageObject
+	{
+		private IWebDriver _webDriver;
+
+
+		public MainGooglePageObject(IWebDriver webDriver)
+		{
+			_webDriver = webDriver;
+		}
+
+		//public void
+
+	}
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,18 +1,22 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
 
 namespace TestProject1
 {
 	public class Tests
 	{
 		private IWebDriver driver;
-
+				
 		[SetUp]
 		public void Setup()
 		{
 			driver = new OpenQA.Selenium.Chrome.ChromeDriver();
 			driver.Navigate().GoToUrl("https://www.google.com/");
 			driver.Manage().Window.Maximize();
+			driver.FindElement(By.Name("q")).SendKeys("picture" + Keys.Enter);			
 		}
 
 		[Test]
